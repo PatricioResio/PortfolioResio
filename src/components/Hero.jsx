99a0bfe68819caeba5d0a39270a0dbcedf471cc4
Patricio Resio <patricio.resio@gmail.com>
2026-05-19 12:41:44 -0300

    fixing Hero position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       
       <ComputersCanvas/>
   
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex 
+      <div className='absolute xs:bottom-20 bottom-20 w-full flex 
       justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,8 +23,8 @@ const Computers = ( {isMobile} ) => {
     />
     <primitive
       object={computer.scene}
-      scale={isMobile ? 0.7 : 0.75}
-      position={isMobile ? [0, -3, -2.2] :  [0, -3.25, -1.5]}
+      scale={isMobile ? 0.50 : 0.65}
+      position={isMobile ? [0, -1, -2.2] :  [0, -2.40, -1.5]}
       rotation={[-0.01, -0.2, -0.1]}
     />
    </mesh>
